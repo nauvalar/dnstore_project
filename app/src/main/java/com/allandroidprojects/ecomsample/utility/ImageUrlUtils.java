2eecec0c990d5e43124fd6fbbd177863b0229a0e
nauvalar <cahbontang97@gmail.com>
2018-12-15 23:50:16 +0800

    Initial commit

---
+++ app/src/main/java/com/allandroidprojects/ecomsample/utility/ImageUrlUtils.java
@@ -0,0 +1,175 @@
+package com.allandroidprojects.ecomsample.utility;
+
+import java.util.ArrayList;
+
+/**
+ * Created by 06peng on 2015/6/24.
+ */
+public class ImageUrlUtils {
+    static ArrayList<String> wishlistImageUri = new ArrayList<>();
+    static ArrayList<String> cartListImageUri = new ArrayList<>();
+
+    public static String[] getImageUrls() {
+        String[] urls = new String[] {
+                "https://indo.bestpriceupdate.com/id/1050/products/original/7016ca07a86e0215f51b5b8f62301a8d.jpg",
+                "https://ae01.alicdn.com/kf/HTB13n8_zb1YBuNjSszeq6yblFXaY/aigo-dr12-8pcs-computer-case-pc-fan-rgb-dual-led-ring-120mm-silent-IR-remote-controller.jpg",
+                "https://ae01.alicdn.com/kf/HTB1TutUuwKTBuNkSne1q6yJoXXap/AIGO-DR12-3pcs-Computer-Case-PC-Cooling-Fan-RGB-Adjust-LED-120mm-Quiet-IR-Remote-High.jpg",
+                "http://www.batamonlineshop.com/media/catalog/product/cache/1/thumbnail/500x/17f82f742ffe127f42dca9de82fb58b1/v/a/valborg_white_1.jpg",
+                "http://www.klikgalaxy.com/image-product/img10025-1540363138.jpg",
+                "https://ms-start.com/EasyEdit/UserFiles/Catalog/msstart-ms-monster-midi-tower-gaming-kuciste-plavi-led-ventilator/msstart-ms-monster-midi-tower-gaming-kuciste-plavi-led-ventilator-slika_1012_1012.jpeg",
+                "https://www.17tahun.biz.id/details/350/images/100/MTA-2288746/aigo_aigo-dr12-fan--5-fan-_full03.jpg",
+                "http://www.klikgalaxy.com/image-product/img9428-1537352809.jpg",
+                "http://www.gamemaster.vn/wp-content/uploads/2017/10/Mainboard-Gigabyte-Z370-Gaming-3-5.png",
+                "http://www.hongha.asia/Upload/images/GIGABYTE%20B360M%20AORUS%20Gaming%203.jpg",
+                "https://cf4.s3.souqcdn.com/item/2018/02/26/12/11/94/83/item_XL_12119483_113044183.jpg",
+                "https://5.imimg.com/data5/YC/BT/MY-22772857/universal-home-theatre-500x500.jpg",
+                "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/4784/4784802_sa.jpg",
+                "https://images-na.ssl-images-amazon.com/images/I/91mR9%2BYkZgL._SX425_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/I/81mP%2BsrYO9L._SX425_.jpg",
+                "https://asset.msi.com/resize/image/global/product/product_1_20170302180050_58b7ed521f873.png62405b38c58fe0f07fcef2367d8a9ba1/600.png",
+                "https://static.gigabyte.com/Product/2/6504/2018011715221813_big.png",
+                "https://vkehe45v84w20n29n1m63wok-wpengine.netdna-ssl.com/wp-content/uploads/2018/11/ASRock-H370M-PRO4-LGA1151-Motherboard.jpg",
+                "https://www.webantics.com/content/images/thumbs/004/0045292_msi-b150-gaming-m3-atx-gaming-motherboard_600.png",
+                "http://3.bp.blogspot.com/-Rr9mubONn9M/VQcdAWePr1I/AAAAAAAAALM/0g66RuISsVg/s1600/Tarjeta-madre-Z77-FTW-de-EVGA.jpg",
+                "https://images-na.ssl-images-amazon.com/images/I/51k0l-O2gqL.jpg",
+                "https://www.gamingscan.com/wp-content/uploads/2017/12/ram-for-gaming-450x305.jpg",
+                "https://www.gamingscan.com/wp-content/uploads/2017/11/best-ram-for-gaming-2018.jpg",
+                "https://vkehe45v84w20n29n1m63wok-wpengine.netdna-ssl.com/wp-content/uploads/2016/11/G.SKILL-Ripjaws-X-series-8GB.jpg",
+                "https://www.scan.co.uk/images/products/2836084-a.jpg",
+                "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/5980/5980201cv14d.jpg",
+                "https://assets.hardwarezone.com/img/2017/03/VR_00.jpg",
+                "https://www.laptopsdirect.co.uk/Images/106AMK0.0001_1_LargeProductImage.jpg?width=700&height=700&v=1",
+                "https://www.dhresource.com/0x0s/f2-albu-g6-M01-AB-78-rBVaR1qiBH2AXr_7AB9VCfZk4yc757.jpg/gamepower-atlas-gaming-pc-case-with-led-fan.jpg",
+                "https://ttpremium.com/wp-content/uploads/2016/11/gk.jpg",
+                "https://sta3-nzxtcorporation.netdna-ssl.com/uploads/product_image/image/1734/large_d727db1301c29998.jpg",
+                "https://tshop.r10s.com/6a2/579/573e/aca0/30dc/9fc6/e52c/11c9e8aab8c4544489145b.JPG",
+                "http://heteroku.com/heteroku-upload/a7d189b0400e2cff35693f530d1f35d8.jpg",
+                "https://ae01.alicdn.com/kf/HTB1fc.gNpXXXXbXXXXXq6xXFXXXK/Dream-Color-RGB-LED-Lighting-Kit-for-Gaming-PC-Computer-Case-SK6812-WS2812B-Magnets-Built-in.jpg_640x640.jpg",
+                "https://image.pushauction.com/0/0/e00034fb-f25f-4a47-bde4-dfee874aa54f/eb23849d-a2b9-432a-8144-b504f73f4a51.jpg"
+    };
+        return urls;
+    }
+
+    public static String[] getOffersUrls() {
+        String[] urls = new String[]{
+                "https://assets.razerzone.com/eeimages/banners/razerstore-og.jpg",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_bZefC0Oijglc4jWAt_oB0Pae1-zrjZQvZAV-IWm8M0-iTc7C",
+                "https://cdn.shopify.com/s/files/1/2296/7833/products/product-image-355037285_x700.jpg?v=1504234956",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrgMfSGnoGIOANDZrphaNpXeHZ0nKCcZN-9lTF0JDCLg0OfRFy",
+                "https://www.hardwareheaven.com/wp-content/uploads/2016/05/razer-overwatch-peripherals-bundle.jpg",
+                "https://id-live-01.slatic.net/original/3a2ed132fcecffdff4f3a359bebbc753.jpg",
+                "https://www.murdockcruz.com/wp-content/uploads/2018/08/konsol-Xbox-e1535075605314.jpg",
+                "http://www.batamonlineshop.com/media/homeslider/small/gaming-gear.jpg",
+                "https://s0.bukalapak.com/img/0289483883/w-1000/Paket_1_Sades_Gaming_Gear.jpg",
+                "https://www.pemmz.com/media/catalog/product/cache/5/image/9df78eab33525d08d6e5fb8d27136e95/r/o/roccat_keyboard_suora_fx_gaming_mouse_kiro_headset_khan_pro.png"
+        };
+        return urls;
+    }
+
+    public static String[] getHomeApplianceUrls() {
+        String[] urls = new String[]{
+                "http://4k.com/wp-content/uploads/2015/07/TITAN-Z-angle.jpg",
+                "https://www.scan.co.uk/images/products/2784518-a.jpg",
+                "https://images.anandtech.com/doci/12595/asrock_final_graphics_678_678x452.jpg",
+                "https://www.techpowerup.com/img/14-11-06/19e.jpg",
+                "https://www.quietpc.com/images/products/asus-rog-strix-gtx1070ti-a8g-g-box-large.jpg",
+                "https://www.guphotos.com/images/uploads/C3632-7.jpg",
+                "https://asset.msi.com/resize/image/global/product/product_7_20160309182104_56dff910f1378.png62405b38c58fe0f07fcef2367d8a9ba1/600.png",
+                "https://www.scan.co.uk/images/products/2933690-a.jpg",
+                "https://www.dhresource.com/0x0s/f2-albu-g6-M01-A3-6F-rBVaSFvG8MSANsVKAAPXvx45IVw068.jpg/2018-new-asus-strix-rtx2070-o8g-gaming-gpu.jpg",
+                "https://matrixwarehouse.co.za/3487-large_default/asus-strix-gtx1060-6gb-gaming-gpu.jpg",
+                "https://www.scan.co.uk/images/products/2570822-a.jpg",
+                "https://static.raru.co.za/cover/2016/05/23/4726958-l.jpg?v=1465548300",
+                "https://www.wootware.co.za/media/catalog/product/cache/1/image/1024x768/9df78eab33525d08d6e5fb8d27136e95/8/1/81jwmazpfql._sl1500_.jpg",
+                "https://www.gamingscan.com/wp-content/uploads/2018/11/gpu.jpg",
+                "https://images-eu.ssl-images-amazon.com/images/I/719BM+soIqL._AC_SS350_.jpg",
+                "https://www.bhphotovideo.com/images/images750x750/sapphire_21276_00_20g_radeon_rx_vega_56_1361413.jpg"
+        };
+        return urls;
+    }
+
+    public static String[] getElectronicsUrls() {
+        String[] urls = new String[]{
+                "https://asset.msi.com/resize/image/global/product/product_6_20180212174221_5a81617d55d78.png62405b38c58fe0f07fcef2367d8a9ba1/1024.png",
+                "https://asset.msi.com/resize/image/global/product/product_4_20180308171427_5aa0fef3ababa.png62405b38c58fe0f07fcef2367d8a9ba1/1024.png",
+                "https://asset.msi.com/resize/image/global/product/product_10_20180308171435_5aa0fefb18bbd.png62405b38c58fe0f07fcef2367d8a9ba1/1024.png",
+                "https://asset.msi.com/resize/image/global/product/product_0_20180308102404_5aa09ec4830b6.png62405b38c58fe0f07fcef2367d8a9ba1/1024.png",
+                "https://asset.msi.com/resize/image/global/product/product_4_20180308102402_5aa09ec224551.png62405b38c58fe0f07fcef2367d8a9ba1/1024.png",
+                "http://i.dell.com/sites/imagecontent/videos/en/PublishingImages/aw-17-gaming.jpg",
+                "http://laptopno1.com/Upload/Img/Products/Lenovo%20Y520.png",
+                "https://www.progamerreview.com/wp-content/uploads/2018/03/1-19.jpg",
+                "https://www.originpc.com/gaming/laptops/eon15-s-thin/img/intro.png",
+                "https://cdn.vox-cdn.com/thumbor/KXnq_57z-ftQ4uXP_NGXL9LfJlw=/0x0:2040x1360/1200x0/filters:focal(0x0:2040x1360):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/11564905/akrales_180618_2661_0060.jpg",
+                "https://www.bestgaminglaptops.co.uk/wp-content/uploads/2017/05/799-HP-OMEN-15-AX007NS-i5-Cheap-budget-gaming-laptop-under-800.jpg",
+                "https://souqcms.s3.amazonaws.com/spring/images/2018/hp/15-cb005ne/3-hp-pavilion-gaming-laptop-core-i7-7700hq-15.6-inch-fhd-acid-green.jpg",
+                "https://images-na.ssl-images-amazon.com/images/I/81EEuUyk6yL._SX425_.jpg"
+        };
+        return urls;
+    }
+
+    public static String[] getLifeStyleUrls() {
+        String[] urls = new String[]{
+                "https://brain-images-ssl.cdn.dixons.com/3/8/10158183/u_10158183.jpg",
+                "https://azcd.harveynorman.com.au/media/catalog/product/cache/21/image/992x558/9df78eab33525d08d6e5fb8d27136e95/g/4/g402-hyperion-fury-ultra-fast-fps-gaming-mouse_1.jpg",
+                "https://www.prohavit.com/wp-content/uploads/hv-ms794-programmable-gaming-mouse.jpg",
+                "https://images-na.ssl-images-amazon.com/images/I/71rXY6v4DRL._SX466_.jpg",
+                "https://cf2.s3.souqcdn.com/item/2016/01/17/99/03/79/8/item_XL_9903798_11848910.jpg",
+                "https://www.evetech.co.za/repository/ProductImages/steelseries-rival-600-optical-gaming-mouse-1000px-v1-0007.jpg",
+                "https://www.prohavit.com/wp-content/uploads/hv-kb380l-gaming-keyboard.jpg",
+                "https://multimedia.bbycastatic.ca/multimedia/products/500x500/105/10567/10567347.jpg",
+                "https://content.ebgames.com.au/website/images/detail/206424_detail.jpg",
+                "https://blossomzones.com/wp-content/uploads/2017/05/G-PRO-KEYB-TKL.jpg",
+                "https://s.blanja.com/picspace/367/136559/999.999_daff93835d4943f7be774ce0e39e5ed2.jpg",
+                "https://media.hitekno.com/thumbs/2018/05/24/42338-keyboard-gamersdericksoft/730x480-img-42338-keyboard-gamersdericksoft.jpg",
+                "https://cdns.klimg.com/merdeka.com/i/w/news/2013/06/25/210784/670x335/jajaran-keyboard-gaming-terbaru-logitech-g-diluncurkan.jpg",
+                "https://www.wootware.co.za/media/catalog/product/cache/1/image/1024x768/9df78eab33525d08d6e5fb8d27136e95/f/r/fr_hs-hc2kr.jpg",
+                "http://ae01.alicdn.com/kf/HTB1qKlBwyCYBuNkSnaVq6AMsVXaC.jpg",
+                "https://images-na.ssl-images-amazon.com/images/I/81xV008w5EL._SL1500_.jpg",
+                "https://c1.neweggimages.com/NeweggImage/ProductImage/26-249-226-V01.jpg",
+        };
+        return urls;
+    }
+
+    public static String[] getBooksUrls() {
+        String[] urls = new String[]{
+                "https://images-na.ssl-images-amazon.com/images/I/81Zt42ioCgL._SX425_.jpg",
+                "https://www.lg.com/us/images/monitors/md05806529/24GM79G-350x350.jpg",
+                "https://assets.pcmag.com/media/images/444996-dell-24-gaming-monitor-s2417dg.jpg?width=810&height=456",
+                "https://multimedia.bbycastatic.ca/multimedia/products/500x500/112/11292/11292475.jpg",
+                "https://www.lg.com/us/images/monitors/md05897716/27GK750F-B-350x350.jpg",
+                "https://www.euronics.ee/UserFiles/Products/Images/162057-lg-24mp59g-p-fhd-monitor-1.png",
+                "https://azcd.harveynorman.com.au/media/catalog/product/cache/21/image/992x558/9df78eab33525d08d6e5fb8d27136e95/u/m/umcx0sap02.jpg",
+                "https://viotek.com/wp-content/uploads/61lsMGP1GrL._SL1000_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/I/719DEG6ggvL._SX425_.jpg",
+                "https://edgeup.asus.com/wp-content/uploads/2018/06/vg49v-angle.jpg",
+                "https://brain-images-ssl.cdn.dixons.com/8/3/10141938/u_10141938.jpg",
+                "https://www.mwave.com.au/images/400/AC11200_4.jpg",
+                "https://prod.scorptec.com.au/18/991/70659/146506_large.jpg",
+                "https://asset.msi.com/global/picture/images/monitor/Gaming_monitor.png",
+                "https://www.pemmz.com/media/catalog/product/cache/5/small_image/300x300/9df78eab33525d08d6e5fb8d27136e95/3/_/3_7_2.png",
+        };
+        return urls;
+    }
+
+    // Methods for Wishlist
+    public void addWishlistImageUri(String wishlistImageUri) {
+        this.wishlistImageUri.add(0,wishlistImageUri);
+    }
+
+    public void removeWishlistImageUri(int position) {
+        this.wishlistImageUri.remove(position);
+    }
+
+    public ArrayList<String> getWishlistImageUri(){ return this.wishlistImageUri; }
+
+    // Methods for Cart
+    public void addCartListImageUri(String wishlistImageUri) {
+        this.cartListImageUri.add(0,wishlistImageUri);
+    }
+
+    public void removeCartListImageUri(int position) {
+        this.cartListImageUri.remove(position);
+    }
+
+    public ArrayList<String> getCartListImageUri(){ return this.cartListImageUri; }
+}
